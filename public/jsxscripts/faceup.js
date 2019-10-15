f124340f37a71aa808874ccc424e189d55af17c8
simplyking-decadev <56389903+simplyking-decadev@users.noreply.github.com>
2019-10-15 20:47:56 +0100

    fix removed fruit highlighting

--- public/jsxscripts/faceup.js
+++ public/jsxscripts/faceup.js
@@ -1,4 +1,6 @@
 const fruitsAlpha = ["orange", "apple", "watermelon", "pineapple", "mango", "pawpaw", "strawberry", "guava", "melon", "banana", "orange", "apple", "watermelon", "pineapple", "mango", "pawpaw", "strawberry", "guava", "melon", "banana", "orange", "apple", "watermelon", "pineapple", "mango", "pawpaw", "strawberry", "guava", "melon", "banana", "orange", "apple", "watermelon", "pineapple", "mango", "pawpaw", "strawberry", "guava", "melon", "banana"]
+const filtered_fruit_marks = ["good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good"]
+const disable_filtered_fruits_marks = [true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true]
 function shuffle(array){
         var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -19,27 +21,38 @@ function shuffle(array){
     }
 const fruitType = ["orange", "apple", "watermelon", "pineapple", "mango", "pawpaw", "strawberry", "guava", "melon", "banana"]
 var shuffled_fruits=shuffle(fruitsAlpha);
-const fruits = shuffled_fruits.map(each_fruit => "./images/" + each_fruit + ".png")
+//save the shuffled_fruits to local storage
+var saved_shuffled_fruits=JSON.parse(localStorage.getItem("shuffled_fruits"))
+if(saved_shuffled_fruits==null){
+    var json_save_shuffle_fruit=JSON.stringify(shuffled_fruits);
+    localStorage.setItem("shuffled_fruits",json_save_shuffle_fruit)
+}
+var shuffled_fruits_json = JSON.parse(localStorage.getItem("shuffled_fruits"))
+const fruits = shuffled_fruits_json.map(each_fruit => "./images/" + each_fruit + ".png")
+const removed_fruits_image=filtered_fruit_marks.map(each=>"btn-float btn-pulse red");
 const fruitsIndex=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39]
 
 var groupedFruits = [];
 var temp = [];
         for (let i = 0; i < fruitType.length; i++) {
-            for (let j = 0; j < shuffled_fruits.length; j++) {
+            for (let j = 0; j < shuffled_fruits_json.length; j++) {
 
-                if (fruitType[i] === shuffled_fruits[j]){
+                if (fruitType[i] === shuffled_fruits_json[j]){
                     temp.push(j)
                 }
             }
             groupedFruits.push(temp)
             temp = []
         }
-
 function checkFruit(fru){
+    var existing_fruits_deleted=JSON.parse(localStorage.getItem("removed_fruits"));
+    if(existing_fruits_deleted==null) existing_fruits_deleted=[];
     var val = fru.filter(e => e !== null);
     var fruitsPlayed = val.map(e => e * 1);
     var returnFruits=[]
+    var removedFruits=[]
     var counter = []
+    
     for (let p = 0; p < groupedFruits.length; p++) {
         for (let x = 0; x < groupedFruits[p].length; x++) {
             if (fruitsPlayed.includes(groupedFruits[p][x])) {
@@ -48,8 +61,17 @@ function checkFruit(fru){
         }
         if (counter.length >= 2) {
             var new_fruits_left = fruitsPlayed.filter(e => !counter.includes(e))
+            var removed_fruits = fruitsPlayed.filter(e => counter.includes(e))
             for(let m=0;m<new_fruits_left.length;m++){
                 returnFruits[new_fruits_left[m]]=new_fruits_left[m];
+            }
+            for (var k = 0; k < removed_fruits.length; k++) {
+                removedFruits[removed_fruits[k]] = removed_fruits[k];
+                existing_fruits_deleted[removed_fruits[k]]=removed_fruits[k];
+            }
+            if(existing_fruits_deleted.length>0){
+                var json_fruits = JSON.stringify(existing_fruits_deleted)
+                localStorage.setItem("removed_fruits", json_fruits);
             }
             
             return returnFruits;
@@ -58,12 +80,13 @@ function checkFruit(fru){
         
         counter = []
         returnFruits=[]
+        removedFruits=[]
     }
     return fru;
 }
 
 const className="btn btn-pulse white";
-
+const fb="fa fa-facebook"
 
 class FruitButtons extends React.Component {
 
@@ -80,9 +103,15 @@ class FruitButtons extends React.Component {
     render(){
         var num_display=this.props.buttonIndex;
         var fruit_num=this.props.display;
+        var removed_fruits=localStorage.getItem("removed_fruits");
+        var removed_fruits_json=JSON.parse(removed_fruits);
+        if(!removed_fruits_json){
+            removed_fruits_json=[];
+        }
+      
         return (
             <div>
-                {num_display.map(e => <button key={e} value={e} className={className} onClick={this.openFruit} ><img src={fruits[fruit_num[e]]} /></button>)}
+                {num_display.map(e => <button disabled={disable_filtered_fruits_marks[removed_fruits_json[e]]} className={removed_fruits_image[removed_fruits_json[e]]} key={e} value={e} onClick={this.openFruit} ><img src={fruits[fruit_num[e]]} /></button>)}
             </div>
         )
     }
@@ -165,7 +194,7 @@ class FruitFaceGame extends React.Component{
         
         return(
             <div>
-                <FruitButtons buttonIndex={fruitsIndex} openFruit={this.openFruit} display={fruit_numbers} keep={fruit_numbers}/>
+                <FruitButtons buttonIndex={fruitsIndex} openFruit={this.openFruit} display={fruit_numbers}/>
             </div>
 
         )
--- public/reactjs/app.js
+++ public/reactjs/app.js
@@ -9,6 +9,8 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var fruitsAlpha = ["orange", "apple", "watermelon", "pineapple", "mango", "pawpaw", "strawberry", "guava", "melon", "banana", "orange", "apple", "watermelon", "pineapple", "mango", "pawpaw", "strawberry", "guava", "melon", "banana", "orange", "apple", "watermelon", "pineapple", "mango", "pawpaw", "strawberry", "guava", "melon", "banana", "orange", "apple", "watermelon", "pineapple", "mango", "pawpaw", "strawberry", "guava", "melon", "banana"];
+var filtered_fruit_marks = ["good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good", "good"];
+var disable_filtered_fruits_marks = [true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true];
 function shuffle(array) {
     var currentIndex = array.length,
         temporaryValue,
@@ -31,25 +33,36 @@ function shuffle(array) {
 }
 var fruitType = ["orange", "apple", "watermelon", "pineapple", "mango", "pawpaw", "strawberry", "guava", "melon", "banana"];
 var shuffled_fruits = shuffle(fruitsAlpha);
-var fruits = shuffled_fruits.map(function (each_fruit) {
+//save the shuffled_fruits to local storage
+var saved_shuffled_fruits = JSON.parse(localStorage.getItem("shuffled_fruits"));
+if (saved_shuffled_fruits == null) {
+    var json_save_shuffle_fruit = JSON.stringify(shuffled_fruits);
+    localStorage.setItem("shuffled_fruits", json_save_shuffle_fruit);
+}
+var shuffled_fruits_json = JSON.parse(localStorage.getItem("shuffled_fruits"));
+var fruits = shuffled_fruits_json.map(function (each_fruit) {
     return "./images/" + each_fruit + ".png";
+});
+var removed_fruits_image = filtered_fruit_marks.map(function (each) {
+    return "btn-float btn-pulse red";
 });
 var fruitsIndex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39];
 
 var groupedFruits = [];
 var temp = [];
 for (var i = 0; i < fruitType.length; i++) {
-    for (var j = 0; j < shuffled_fruits.length; j++) {
+    for (var j = 0; j < shuffled_fruits_json.length; j++) {
 
-        if (fruitType[i] === shuffled_fruits[j]) {
+        if (fruitType[i] === shuffled_fruits_json[j]) {
             temp.push(j);
         }
     }
     groupedFruits.push(temp);
     temp = [];
 }
-
 function checkFruit(fru) {
+    var existing_fruits_deleted = JSON.parse(localStorage.getItem("removed_fruits"));
+    if (existing_fruits_deleted == null) existing_fruits_deleted = [];
     var val = fru.filter(function (e) {
         return e !== null;
     });
@@ -57,7 +70,9 @@ function checkFruit(fru) {
         return e * 1;
     });
     var returnFruits = [];
+    var removedFruits = [];
     var counter = [];
+
     for (var p = 0; p < groupedFruits.length; p++) {
         for (var x = 0; x < groupedFruits[p].length; x++) {
             if (fruitsPlayed.includes(groupedFruits[p][x])) {
@@ -68,8 +83,19 @@ function checkFruit(fru) {
             var new_fruits_left = fruitsPlayed.filter(function (e) {
                 return !counter.includes(e);
             });
+            var removed_fruits = fruitsPlayed.filter(function (e) {
+                return counter.includes(e);
+            });
             for (var m = 0; m < new_fruits_left.length; m++) {
                 returnFruits[new_fruits_left[m]] = new_fruits_left[m];
+            }
+            for (var k = 0; k < removed_fruits.length; k++) {
+                removedFruits[removed_fruits[k]] = removed_fruits[k];
+                existing_fruits_deleted[removed_fruits[k]] = removed_fruits[k];
+            }
+            if (existing_fruits_deleted.length > 0) {
+                var json_fruits = JSON.stringify(existing_fruits_deleted);
+                localStorage.setItem("removed_fruits", json_fruits);
             }
 
             return returnFruits;
@@ -77,11 +103,13 @@ function checkFruit(fru) {
 
         counter = [];
         returnFruits = [];
+        removedFruits = [];
     }
     return fru;
 }
 
 var className = "btn btn-pulse white";
+var fb = "fa fa-facebook";
 
 var FruitButtons = function (_React$Component) {
     _inherits(FruitButtons, _React$Component);
@@ -108,13 +136,19 @@ var FruitButtons = function (_React$Component) {
 
             var num_display = this.props.buttonIndex;
             var fruit_num = this.props.display;
+            var removed_fruits = localStorage.getItem("removed_fruits");
+            var removed_fruits_json = JSON.parse(removed_fruits);
+            if (!removed_fruits_json) {
+                removed_fruits_json = [];
+            }
+
             return React.createElement(
                 "div",
                 null,
                 num_display.map(function (e) {
                     return React.createElement(
                         "button",
-                        { key: e, value: e, className: className, onClick: _this2.openFruit },
+                        { disabled: disable_filtered_fruits_marks[removed_fruits_json[e]], className: removed_fruits_image[removed_fruits_json[e]], key: e, value: e, onClick: _this2.openFruit },
                         React.createElement("img", { src: fruits[fruit_num[e]] })
                     );
                 })
@@ -205,7 +239,7 @@ var FruitFaceGame = function (_React$Component2) {
             return React.createElement(
                 "div",
                 null,
-                React.createElement(FruitButtons, { buttonIndex: fruitsIndex, openFruit: this.openFruit, display: fruit_numbers, keep: fruit_numbers })
+                React.createElement(FruitButtons, { buttonIndex: fruitsIndex, openFruit: this.openFruit, display: fruit_numbers })
             );
         }
     }]);
